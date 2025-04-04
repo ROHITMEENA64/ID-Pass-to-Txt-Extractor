fd8accb01f58f4cbb47a8de59f6ef79a036bc833
ROHITMEENA64 <157285469+ROHITMEENA64@users.noreply.github.com>
2025-04-04 18:04:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 api_id =27775431
 api_hash = "b70bb1d45a1d05236671d4cc615e40f9"
 bot_token = ""
-auth_users = "46513214"
+auth_users = "6414266397"
 sudo_users = [int(num) for num in auth_users.split(",")]
 osowner_users = "6414266397"
 owner_users = [int(num) for num in osowner_users.split(",")]
